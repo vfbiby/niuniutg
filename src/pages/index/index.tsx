f11de7073bd1681ac2d9b4e0e780a9f4c7e9e10a
Musk Zhang <muskzhang2024@gmail.com>
2024-10-06 23:52:14 +0800

    add nutui icons

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,10 @@
 import {View, Text} from '@tarojs/components'
 import {useLoad} from '@tarojs/taro'
-import {AtButton, AtIcon} from "taro-ui";
+import {Cart, Category, Find, Home, User} from '@nutui/icons-react-taro'
 
-import {Button} from "@nutui/nutui-react-taro";
+import {Button, Divider, Tabbar} from "@nutui/nutui-react-taro";
 
+import {AtDrawer} from "taro-ui";
 import './index.scss'
 
 export default function Index() {
@@ -13,17 +14,22 @@ export default function Index() {
   const marginStyle = {margin: 8}
 
   return (
-    <View className='index'>
-      <Text>Hello world!</Text>
-      <AtButton type='primary'>按钮文案s</AtButton>
-      <AtIcon value='clock' size='30' color='#F00'></AtIcon>
-      <Button openType='share' style={marginStyle}>
-        Share
-      </Button>
-      <Button openType='openSetting' style={marginStyle}>
-        打开授权设置页
-      </Button>
-      <Button openType='share'>goods</Button>
+    <View className='index' style={{height: "100%"}}>
+      <View style={{height: "90vh"}}>
+        <Text>Hello world!</Text>
+        <Divider />
+        <Button type='primary' openType='share' style={marginStyle}>
+          Share
+        </Button>
+      </View>
+      <Tabbar fixed>
+        <Tabbar.Item title='首页' icon={<Home size={18} />} />
+        <Tabbar.Item title='分类' icon={<Category size={18} />} />
+        <Tabbar.Item title='发现' icon={<Find size={18} />} />
+        <Tabbar.Item title='购物车' icon={<Cart size={18} />} />
+        <Tabbar.Item title='我的' icon={<User size={18} />} />
+      </Tabbar>
+      <AtDrawer right mask items={['菜单1', '菜单2']} show={false}></AtDrawer>
     </View>
   )
 }
