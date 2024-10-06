e409a991bda036c54a5c746f91c7a42ec69f0049
Musk Zhang <muskzhang2024@gmail.com>
2024-10-06 19:40:14 +0800

    integrate nutui

--- config/index.ts
+++ config/index.ts
@@ -8,7 +8,15 @@ export default defineConfig<'vite'>(async (merge, { command, mode }) => {
   const baseConfig: UserConfigExport<'vite'> = {
     projectName: 'niuniutg-taro4',
     date: '2024-10-6',
-    designWidth: 750,
+    plugins: ['@tarojs/plugin-html'],
+    designWidth (input) {
+      // 配置 NutUI 375 尺寸
+      if (input?.file?.replace(/\\+/g, '/').indexOf('@nutui') > -1) {
+        return 375
+      }
+      // 全局使用 Taro 默认的 750 尺寸
+      return 750
+    },
     deviceRatio: {
       640: 2.34 / 2,
       750: 1,
@@ -17,7 +25,6 @@ export default defineConfig<'vite'>(async (merge, { command, mode }) => {
     },
     sourceRoot: 'src',
     outputRoot: 'dist',
-    plugins: [],
     defineConstants: {
     },
     copy: {
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,19 +1,30 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
+import {View, Text} from '@tarojs/components'
+import {useLoad} from '@tarojs/taro'
 import {AtButton, AtIcon} from "taro-ui";
+import '@nutui/nutui-react-taro/dist/style.css'
+
+import {Button} from "@nutui/nutui-react-taro";
 
 import './index.scss'
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
     console.log('Page loaded.')
   })
+  const marginStyle = {margin: 8}
 
   return (
     <View className='index'>
       <Text>Hello world!</Text>
       <AtButton type='primary'>按钮文案s</AtButton>
       <AtIcon value='clock' size='30' color='#F00'></AtIcon>
+      <Button openType='share' style={marginStyle}>
+        Share
+      </Button>
+      <Button openType='openSetting' style={marginStyle}>
+        打开授权设置页
+      </Button>
+      <Button>goods</Button>
     </View>
   )
 }
